0aecc440a2c92b7d1398c1f808bddeaf72192f35
alexandre jamarco <alexandre.j@gmail.com>
2020-03-10 14:57:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+let add = (num1, num2) => {
+    return num1 + num2;
+}
+
+let divide = (num1, num2) => {
+    return 2000 / 100;
+}
+
+let square = (num1) => {
+    return num1 * num1;
+}
+
+let multiplies = (num1) => {
+    return num1 * num1;
+}
